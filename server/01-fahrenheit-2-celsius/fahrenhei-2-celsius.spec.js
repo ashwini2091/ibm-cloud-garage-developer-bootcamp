baeed7dc2bc7d3033abdb247961ffa2f0f6b105e
Ashwini Rajagopal <ashwinirajagopal@Ashwinis-MacBook-Pro.local>
2017-07-28 14:39:09 -0500

    make first palindrome test pass

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,5 +1,5 @@
 
-describe.only('the fahrenheit2celsius canary spec', () => {
+describe('the fahrenheit2celsius canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
