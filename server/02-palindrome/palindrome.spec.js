2e6a6e08733e395d165db0b5eb559836f5186070
Ashwini Rajagopal <ashwinirajagopal@Ashwinis-MacBook-Pro.local>
2017-07-28 15:46:39 -0500

    made 7 test cases of plaindrome pass

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,11 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome(word){
-      return word.split('').reverse().join('') === word;
+  function isPalindrome(phrase){
+      if ( phrase.trim().length > 0) {
+        return phrase.split('').reverse().join('') === phrase;
+      }
+      return false;
   }
 
   describe('a palindrome', () => {
@@ -14,9 +17,17 @@ describe.only('the palindrome canary spec', () => {
     it('is not racecars', () => {
       isPalindrome('racecars').should.be.false();
     });
-    it('is not race car');
-    it('"  " is not a palindrome');
-    it('"" is not a palindrome');
-    it('is "mom dad mom"');
+    it('is not race car', () => {
+      isPalindrome('race car').should.be.false();
+    });
+    it('"  " is not a palindrome', () => {
+      isPalindrome('  ').should.be.false();
+    });
+    it('"" is not a palindrome', () => {
+      isPalindrome('').should.be.false();
+    });
+    it('is "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
   });
 });
